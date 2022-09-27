1b34b77b01ec8c17c32eedf651793cc88ef13636
Takumi Yoshida <umibooose@gmail.com>
2022-09-27 20:30:15 +0900

    Add tests to `NatureRemo:Endpoints::Users`

---
+++ test/endpoints/users/update_user_me_test.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class UpdateUserMeTest < Minitest::Test
+  def test_update_user_me_success
+    client = NatureRemo::Client.new('token-xxxxx')
+    WebMock.stub_request(:post, 'https://api.nature.global/1/users/me')
+           .with(
+             headers: {
+               'Accept' => '*/*',
+               'Authorization' => 'Bearer token-xxxxx'
+             },
+             body: {
+               nickname: 'String'
+             }
+           )
+           .to_return(
+             status: 200,
+             body: "{\n\"id\": \"3fa85f64-5717-4562-b3fc-2c963f66afa6\",\n\"nickname\": \"String\"\n}"
+           )
+
+    assert_equal "{\n\"id\": \"3fa85f64-5717-4562-b3fc-2c963f66afa6\",\n\"nickname\": \"String\"\n}", client.update_user_me(nickname: 'String')
+  end
+
+  def test_update_user_me_failure
+    client = NatureRemo::Client.new('token-xxxxx')
+    WebMock.stub_request(:post, 'https://api.nature.global/1/users/me')
+           .with(
+             headers: {
+               'Accept' => '*/*',
+               'Authorization' => 'Bearer token-xxxxx'
+             },
+             body: {
+               nickname: 'wrong name'
+             }
+           )
+           .to_return(status: 500, body: 'error')
+
+    error = assert_raises NatureRemo::Error do
+      client.update_user_me(nickname: 'wrong name')
+    end
+
+    assert_equal 'request failed with status code 500, error', error.message
+  end
+
+  def test_update_user_me_timeout
+    client = NatureRemo::Client.new('token-xxxxx')
+    WebMock.stub_request(:post, 'https://api.nature.global/1/users/me')
+           .with(
+             headers: {
+               'Accept' => '*/*',
+               'Authorization' => 'Bearer token-xxxxx'
+             },
+             body: {
+               nickname: 'String'
+             }
+           )
+           .to_timeout
+
+    error = assert_raises Faraday::ConnectionFailed do
+      client.update_user_me(nickname: 'String')
+    end
+
+    assert_equal 'execution expired', error.message
+  end
+end
---
+++ test/endpoints/users/user_me_test.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class UserMeTest < Minitest::Test
+  def test_user_success
+    client = NatureRemo::Client.new('token-xxxxx')
+    WebMock.stub_request(:get, 'https://api.nature.global/1/users/me')
+           .with(
+             headers: {
+               'Accept' => '*/*',
+               'Authorization' => 'Bearer token-xxxxx'
+             }
+           )
+           .to_return(
+             status: 200,
+             body: "{\n\"id\": \"3fa85f64-5717-4562-b3fc-2c963f66afa6\",\n\"nickname\": \"string\"\n}"
+           )
+
+    assert_equal "{\n\"id\": \"3fa85f64-5717-4562-b3fc-2c963f66afa6\",\n\"nickname\": \"string\"\n}", client.user_me
+  end
+
+  def test_user_failure
+    client = NatureRemo::Client.new('token-xxxxx')
+    WebMock.stub_request(:get, 'https://api.nature.global/1/users/me')
+           .with(
+             headers: {
+               'Accept' => '*/*',
+               'Authorization' => 'Bearer token-xxxxx'
+             }
+           )
+           .to_return(status: 500, body: 'error')
+
+    error = assert_raises NatureRemo::Error do
+      client.user_me
+    end
+
+    assert_equal 'request failed with status code 500, error', error.message
+  end
+
+  def test_user_me_timeout
+    client = NatureRemo::Client.new('token-xxxxx')
+    WebMock.stub_request(:get, 'https://api.nature.global/1/users/me')
+           .with(
+             headers: {
+               'Accept' => '*/*',
+               'Authorization' => 'Bearer token-xxxxx'
+             }
+           )
+           .to_timeout
+
+    error = assert_raises Faraday::ConnectionFailed do
+      client.user_me
+    end
+
+    assert_equal 'execution expired', error.message
+  end
+end
