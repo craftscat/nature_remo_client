a4aeb227541df964d87bc576db8cc848212491ab
Takumi Yoshida <umibooose@gmail.com>
2023-08-04 07:14:19 +0900

    Add YARD docs on `NatureRemo:EndpointsEchonetLite`

--- lib/nature_remo/endpoints/echonet_lite.rb
+++ lib/nature_remo/endpoints/echonet_lite.rb
@@ -5,6 +5,8 @@ module NatureRemo
     # Endpoints for ECHONET Lite
     module EchonetLite
       # Fetch the list of ECHONET Lite appliances the user has access to.
+      # @return [String] The list of ECHONET Lite appliances.
+      # @raise [NatureRemo::ServerError] Raise error if resposonse status is NOT success.
       #
       # https://swagger.nature.global/#/default/get_1_echonetlite_appliances
       def echonet_lite_appliances
@@ -13,8 +15,11 @@ module NatureRemo
 
       # Notify Remo E / Remo E lite to refresh one or more ECHONET Lite properties.
       # This endpoint is subject to ECHONET Lite specific rate limiting.
-      # See the rate limiting section of the documentation for more details.
-      # Requires echonetlite.*
+      # See the rate limiting section of the documentation for more details. Requires echonetlite.*
+      # @params appliance_id [String] The ECHONET Lite appliance id.
+      # @params epc [String] Comma separated EPCs in hex.
+      # @return [String] empty object.
+      # @raise [NatureRemo::ServerError] Raise error if resposonse status is NOT success.
       #
       # https://swagger.nature.global/#/default/post_1_echonetlite_appliances__applianceid__refresh
       def refresh_echonet_lite_properties(appliance_id:, epc:)
@@ -23,8 +28,12 @@ module NatureRemo
 
       # Set one ECHONET Lite property. This endpoint is subject to ECHONET Lite specific rate limiting.
       # This endpoint is subject to ECHONET Lite specific rate limiting.
-      # See the rate limiting section of the documentation for more details.
-      # Requires echonetlite.*
+      # See the rate limiting section of the documentation for more details. Requires echonetlite.*
+      # @params appliance_id [String] The ECHONET Lite appliance id.
+      # @params epc [String] EPC in hex string.
+      # @params val [String] Value in hex string. eg: 000000FF
+      # @return [String] empty object.
+      # raise [NatureRemo::ServerError] Raise error if resposonse status is NOT success.
       #
       # https://swagger.nature.global/#/default/post_1_echonetlite_appliances__applianceid__set
       def set_echonet_lite_property(appliance_id:, epc:, val:)
